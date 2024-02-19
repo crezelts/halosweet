127bbc9a10273ed39f7e4dd40917930a841620fb
cudd12 <aa22766684@gmail.com>
2024-02-20 00:12:06 +0900

    update code

--- app/page.js
+++ app/page.js
@@ -81,7 +81,7 @@ function Home() {
             </div>
           </div>
           <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to Ifream<br/> Get some good information here...</h1>
+            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Welcome to Ifream<br/> Get some admirable information here...</h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">Ifream is a platform designed to find and utilize the information you need It provides a variety of websites to satisfy your curiosity and expand your quality of life</p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a href="site" className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Get started</a>
