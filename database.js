0f759ce16a7fc5de1c588982f891498e51d65e14
cudd12 <155295596+cudd12@users.noreply.github.com>
2024-02-19 23:47:17 +0900

    Update database.js

--- database.js
+++ database.js
@@ -1,2 +1,2 @@
-export const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://cudd:C500pQtP808bgBSH@cluster0.exl9lhe.mongodb.net/';
+export const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://cudd:C50OpQtP8O8bgBSH@cluster0.exl9lhe.mongodb.net/';
 export const dbName = process.env.MONGODB_DB || 'ifream';
