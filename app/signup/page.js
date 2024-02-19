997c00c15562b8a98d22d7e6cf4e0fa3a1af189a
cudd12 <155295596+cudd12@users.noreply.github.com>
2024-02-20 00:03:08 +0900

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -39,11 +39,11 @@ function SignUpForm() {
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up for an account</h2> // 수정: 'Sign in'을 'Sign up'으로 변경
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign up for an account</h2> 
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form id="signUpForm" className="space-y-6"> // 수정: 'signInForm'을 'signUpForm'으로 변경
+          <form id="signUpForm" className="space-y-6"> 
             <div>
               <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email address</label>
               <div className="mt-2">
@@ -64,7 +64,7 @@ function SignUpForm() {
             </div>
 
             <div>
-              <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign up</button> // 수정: 'Sign-up'을 'Sign up'으로 변경
+              <button type="submit" className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Sign up</button> 
             </div>
           </form>
 
