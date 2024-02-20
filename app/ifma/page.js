0ffede317fa995688c0d7e6dc3a68e1b5d4f6af1
cudd12 <aa22766684@gmail.com>
2024-02-21 01:40:32 +0900

    dd

--- app/ifma/page.js
+++ app/ifma/page.js
@@ -1,12 +1,12 @@
 // { Artficial intellengence connect }
-import { Helmet } from "react-helmet";
+
 
 function ifma() {
   return (
     <div>
-      <Helmet>
-        <link rel="preload" href="https://brainb.netlify.app/_next/static/media/c9a5bc6a7c948fb0-s.p.woff2" as="font" type="font/woff2" crossorigin />
-      </Helmet>
+
+
+      
     </div>
   )
 }
