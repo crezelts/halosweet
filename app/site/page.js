45c5252b12ffbb1fe716f3aed623b4868a8a01be
cudd12 <aa22766684@gmail.com>
2024-02-21 01:38:57 +0900

    dd

--- app/site/page.js
+++ app/site/page.js
@@ -1,13 +1,11 @@
-import { Helmet } from "react-helmet";
+
 
 
 const site = () => {
   return (
     <div>
 
-      <Helmet>
-        <link rel="preload" href="https://brainb.netlify.app/_next/static/media/c9a5bc6a7c948fb0-s.p.woff2" as="font" type="font/woff2" crossorigin />
-      </Helmet>
+
 
     </div>
   )
