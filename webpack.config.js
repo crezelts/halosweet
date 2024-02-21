c51dfcc0422b9ba8e426e0b9df601da6b29fa154
cudd12 <aa22766684@gmail.com>
2024-02-21 13:50:49 +0900

    update code

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+import path from 'path'
+
+export default {
+  entry: {
+    page1: '/app/page.js',
+    page2: '/app/signup/page.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
