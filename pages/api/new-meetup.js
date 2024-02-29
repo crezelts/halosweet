bfee6d856e983960deabcc09596dea5cac533674
cudd12 <aa22766684@gmail.com>
2024-02-29 21:45:23 +0900

    update code

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,34 +5,18 @@ async function handler(req, res) {
     const data = req.body
     const { email, password } = data
 
-  const client = await MongoClient('mongodb+srv://cudd:C50OpQtP8O8bgBSH@cluster0.exl9lhe.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 10 });
+  const client = await MongoClient.connect('mongodb+srv://cudd:C50OpQtP8O8bgBSH@cluster0.exl9lhe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   
-try {  
-  await client.connect()
   const db = client.db('game')
   const meetupCollection = db.collection('meetup')
 
-  const result = await meetupCollection.findOne({ email, password })
+  const result = await meetupCollection.insertOne(data)
 
-  if (result) {
-    return res.status(400).json({ message: 'Email already exists' })
+  console.log(result)
+
+  client.close()
+  res.status(200).json({ message: 'Meetup inserted!' })
   }
-
-  await meetupCollection.createIndex({ email: 1, password: 1 })
-
-  await meetupCollection.insertOne(data)
-  console.log('Meetup inserted:', data)
-
-  res.status(201).json({ message: 'Meetup inserted!' })
-  } catch (error) {
-    console.error('Error inserting meetup:', error)
-    res.status(500).json({ message: 'Failed to insert meetup' })
-  } finally {
-    await client.close()
-  }
-} else {
-  res.status(405).json({ message: 'Method Not Allowed' })
-}
 }
 
 export default handler
